27862df5fdc5e66d1e240ac111a20ff2987d16a5
Jesper Thörnberg <jesper.thornberg@me.com>
2018-01-04 14:34:02 +0100

    Adds toggle button for settingspane

--- ang-src/src/app/components/member/member.component.ts
+++ ang-src/src/app/components/member/member.component.ts
@@ -16,9 +16,10 @@ export class MemberComponent implements OnInit {
     this.addMember = true;
     this.memberModel = {};
     this.isLoading = false;
-    this.successMessage;
-    this.errorMessage;
     this.modalVisible = false;
+    this.showMembersArea = false;
+    this.settingsText = 'Visa inställningar';
+    this.showMembersArea ? this.settingsText == 'Göm inställningar' : 'Visa inställningar';
     
    }
   public addMember: Boolean;  
@@ -28,13 +29,23 @@ export class MemberComponent implements OnInit {
   public members: any[];
   public memberModel: any;
   public isLoading: Boolean;
-  public successMessage: String;
-  public errorMessage: String;
   public modalVisible: Boolean;
+  public showMembersArea: Boolean;
+  public settingsText: String;
 
   ngOnInit() {
     this.getAllMembers();
   }
+
+  toggleCrudView(){
+    this.showMembersArea = !this.showMembersArea;
+    if (this.showMembersArea) {
+      this.settingsText = 'Göm inställningar';
+    } else {
+      this.settingsText = 'Visa inställningar';
+    }
+  }
+
   getAllMembers(){
     console.log(this.memberModel);
     this._memberService.getAllMembers().subscribe((data) => {
@@ -47,7 +58,6 @@ export class MemberComponent implements OnInit {
   selectedMember(id){
     this.modalVisible = true;
     this.addMember = false;
-    this.successMessage = '';
      this._memberService.getMemberById(id).subscribe((data)=> {
        this.memberModel = data.member;
     });
@@ -55,14 +65,12 @@ export class MemberComponent implements OnInit {
 
   updateMember(){
     this.isLoading = true;
-    this.successMessage = '';
     this._memberService.updateMemberName(this.memberModel).subscribe(
       (data) => {},
       (error) => {},
       () => {
         this.getAllMembers();
         this.isLoading = false;
-        this.successMessage = 'Uppdaterat!';
         this.memberModel = {};
         this.modalVisible = false;
       });
@@ -76,12 +84,11 @@ export class MemberComponent implements OnInit {
         txt = "You pressed OK!";
         this._memberService.deleteMember(this.memberModel._id).subscribe(
           (data) => {console.log('Tjena')},
-          (error) => {this.errorMessage = 'Ooops! Det gick inte att ta bort'},
+          (error) => {},
           () => { 
             this.getAllMembers();
             this.isLoading = false;
             this.memberModel = {};
-            this.successMessage = 'Borttagen!'
             this.addMember = true;
             this.modalVisible = false;
             
@@ -102,16 +109,14 @@ export class MemberComponent implements OnInit {
 
   addNew(){
     this.isLoading = true;
-    this.successMessage = '';
     this.memberModel.priority = this.members.length+1;
     this._memberService.createMember(this.memberModel).subscribe(
       (data) => {console.log('Tjena')},
-      (error) => {this.errorMessage = 'Ooops! Det gick inte att spara'},
+      (error) => {},
       () => { 
         this.getAllMembers();
         this.isLoading = false;
         this.memberModel = {};
-        this.successMessage = 'Klart!';
         this.modalVisible = false;        
       }
     )
